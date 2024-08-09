4a477ccfe632c8c81b8ddeaea22dff8dab883fce
waris <146315879+W-aris@users.noreply.github.com>
2024-08-09 16:05:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+let searchInputEl = document.getElementById("searchInput");
+let searchResultsEl = document.getElementById("searchResults");
+let spinnerEl = document.getElementById("spinner");
+
+function createAndAppendSearchResult(result) {
+    let { link, title, description } = result;
+
+    let resultItemEl = document.createElement("div");
+    resultItemEl.classList.add("result-item");
+
+    let titleEl = document.createElement("a");
+    titleEl.href = link;
+    titleEl.target = "_blank";
+    titleEl.textContent = title;
+    titleEl.classList.add("result-title");
+    resultItemEl.appendChild(titleEl);
+
+    let titleBreakEl = document.createElement("br");
+    resultItemEl.appendChild(titleBreakEl);
+
+    let urlEl = document.createElement("a");
+    urlEl.classList.add("result-url");
+    urlEl.href = link;
+    urlEl.target = "_blank";
+    urlEl.textContent = link;
+    resultItemEl.appendChild(urlEl);
+
+    let linkBreakEl = document.createElement("br");
+    resultItemEl.appendChild(linkBreakEl);
+
+    let descriptionEl = document.createElement("p");
+    descriptionEl.classList.add("link-description");
+    descriptionEl.textContent = description;
+    resultItemEl.appendChild(descriptionEl);
+
+    searchResultsEl.appendChild(resultItemEl);
+}
+
+function displayResults(searchResults) {
+    spinnerEl.classList.add("d-none");
+
+    searchResults.forEach(result => {
+        createAndAppendSearchResult(result);
+    });
+}
+
+function searchWikipedia(event) {
+    if (event.key === "Enter") {
+        spinnerEl.classList.remove("d-none");
+        searchResultsEl.textContent = "";
+
+        let searchInput = searchInputEl.value;
+        let url = "https://apis.ccbp.in/wiki-search?search=" + encodeURIComponent(searchInput);
+        let options = {
+            method: "GET"
+        };
+
+        fetch(url, options)
+            .then(response => response.json())
+            .then(jsonData => {
+                let { search_results } = jsonData;
+                displayResults(search_results);
+            })
+            .catch(error => {
+                console.error("Error fetching data:", error);
+                spinnerEl.classList.add("d-none");
+            });
+    }
+}
+
+searchInputEl.addEventListener("keydown", searchWikipedia);
